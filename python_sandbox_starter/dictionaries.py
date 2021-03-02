1a8ec645d80fa06150f1671f6eecff9e55d1525c
nrod91 <dornlee1991@outlook.com>
2021-03-01 23:04:12 -0500

    Completed dictionaries high level overview

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,29 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+
+# create dict
+
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30
+}
+
+# Use constructor
+#person2 = dict(frist_name='Sara', last_name='Williams')
+
+# Get value
+print(person['first_name'])
+print(person.get('last_name'))
+
+# Add key/value
+person['phone'] = '555-555-5555'
+
+print (person, type(person))
+
+# Get dict keys
+print(person.keys())
+
+# Get dict items
+print(person.items())
