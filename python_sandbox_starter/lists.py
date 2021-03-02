16f3ef627a44e97a3497a9a26247288fd567388f
nrod91 <dornlee1991@outlook.com>
2021-03-01 22:21:43 -0500

    Completed list overview

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,31 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create List
+numbers = [1, 2, 3, 4, 5]
+
+friuts = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+#Use a constructor 
+#numbers2 = list((1,2,3,4,5))
+
+#print(numbers, numbers2)
+
+# Get a value 
+print(friuts[1])
+
+# Get length
+print(len(friuts))
+
+# Append to list
+#friuts.append('Mangos')
+
+# Remove from list 
+#friuts.remove('Grapes')
+
+# Insert into position 
+#friuts.insert(2, 'Strawberries')
+
+print(friuts)
+
+
+
